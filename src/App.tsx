174d19a7c2f172111d175f5f381c71a521ac31e6
shoushacin <ken.chen1231234@gmail.com>
2025-03-13 16:13:23 +0000

    pubb

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,94 @@
-import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router";
+import { PokeApi } from "./pokeapiClient";
 
-export const Detail = () => {
-  const { id }= useParams();
-  return <div className="text-6xl">Dettaglio: {id}</div>
-} 
+interface PokemonCard{
+  id: number;
+  image: string;
+  name: string;
+  types: string[];
+}
+const data= [
+  {
+    id: 1,
+    name: "BULBASAUR",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png ",
+    types: ["poison", "water"],
+  },
+
+  {
+  id: 2,
+  name: "PIKACHU",
+  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png ",
+  types: ["elettric"],
+},
+{
+id: 3,
+  name: "CLEFAIRY",
+  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/35.png",
+  types: ["fire", "water"],
+},
+{
+id: 4,
+  name: "RATTATA",
+  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png ",
+  types: ["fire", "water"],
+},
+{
+id: 5,
+  name: "SQUIRTLE",
+  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png ",
+  types: ["fire", "water"],
+},
+{
+  id: 6,
+    name: "CHARMANDER",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png ",
+    types: ["fire"],
+  },
+];
+
+async function fetchData(): Promise<string[]>{
+  const data = await PokeAPI.getPokemonsList();
+  return data.results.map(item => item.name);
+}
+const typeC: { [key: string]: string} = {
+  fire: "bg-red-300",
+  water: "bg-blue-300",
+  elettric: "bg-yellow-200",
+  poison: "bg-purple-400",
+};
+
+function getTypeColor(type: string){
+  const color= typeC[type];
+  return color;
+}
+
+
+const Card = (props: PokemonCard) => {
+  return  <div className="flex flex-wrap bg-red-200 p-0.1 w-3xs">
+            {props.id} - {props.name}
+            <img src = {props.image}/>
+            <div className="flex flex-wrap bg-red-200 gap-2 p-3">
+              {props.types.map((type) => {
+              return <div className={`p-4 ${getTypeColor(type)}`}>{type}</div>
+             })}
+            </div>
+          </div>
+
+ 
+}
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-  const [title, setTitle] = useState("Chopped-Chen");
-
-  useEffect(()=> {
-    if(count==4){
-    setTitle("AKMED NUR")
-    }
-  },[count])
-
   return (
-    <div className="h-dvh flex flex-col items-center justify-center">
-      <div className="bg-white p-8 rounded-md shadow-lg">
-        <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
-          {title}
-        </h1>
-
-        <h2 className="text-center font-bold text-xl mb-6">Vite + React</h2>
-
-        <div className="flex flex-col items-center space-y-4">
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
-          </button>
-
-          <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-              onClick={() => setTitle("Charizard")}
-          >
-            Cambia
-          </button>
-
-         <Link to="/frontend-rocks/dettaglio/1">Link alla pagina di dettaglio</Link>
-        </div>
-      </div>
+  <div>
+    <div className="flex flex-wrap gap-40 p-50">
+      {data.map ((item) => {
+        return <Card id={item.id} name={item.name} image={item.image} types={item.types} />
+      })}
     </div>
+  </div>
   );
+};
+
+export const Detail = () => {
+  return null
 }
---
+++ src/pokeapiClient.ts
@@ -0,0 +1,5 @@
+import {Pokedex} from "pokeapi-js-wrapper";
+
+export const PokeApi= new Pokedex({
+    limit: 20,
+});
